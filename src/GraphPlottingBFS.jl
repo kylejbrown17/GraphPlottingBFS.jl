9f93fec96096e221ab8f9ca8d189224862fb42d9
kylebrown <kylejbrown17@gmail.com>
2019-07-26 22:01:03 -0700

    Just pushing.

--- src/GraphPlottingBFS.jl
+++ src/GraphPlottingBFS.jl
@@ -13,7 +13,8 @@ export
     step_in,
     step_out,
 	bfs!,
-	plot_graph_bfs
+	plot_graph_bfs,
+    render_grid_graph
 
 @with_kw struct BFS_state
     d::Int     = 0 # current depth
@@ -104,6 +105,59 @@ function plot_graph_bfs(graph,v=0;mode=:leaf_aligned,ϵ=0.000000001,edge_pad=1.1
             stroke("black"),fontsize(10pt), font("futura")),
         (context(),circle(x,y,r*ones(nv(G))),fill(fillcolor)),
         (context(),lines...,stroke("black"))
+    )
+end
+
+"""
+    Tool for rendering paths (sequences of vertices) through a grid environment
+"""
+function render_grid_graph(G,paths::Vector{Vector{Int}}=Vector{Vector{Int}}();
+        width=10,r=0.4,color="gray",pathcolors=["lime","red","blue","violet","orange","cyan"])
+    # vertices
+    x = Vector{Float64}()
+    y = Vector{Float64}()
+    for v in vertices(G)
+        push!(x,get_prop(G,v,:x))
+        push!(y,get_prop(G,v,:y))
+    end
+    x₀ = minimum(x) - r
+    y₀ = minimum(y) - r
+    Δx = maximum(x) + r - x₀
+    Δy = maximum(y) + r - y₀
+    # graphic size
+    set_default_graphic_size((width)cm,(width*Δy/Δx)cm)
+    # edges
+    edge_list = []
+    for e in edges(G)
+        push!(edge_list, [(x[e.src],y[e.src]),(x[e.dst],y[e.dst])])
+    end
+    # paths
+    rendered_paths = []
+    starts = []
+    goals = []
+    while length(pathcolors) < length(paths)
+        pathcolors = [pathcolors..., pathcolors...]
+    end
+    for (p,color) in zip(paths,pathcolors)
+        rpath = []
+        start = circle(x[p[1]],y[p[1]],r*0.8)
+        goal = circle(x[p[end]],y[p[end]],r*0.8)
+        for i in 1:length(p)-1
+            push!(rpath, [(x[p[i]],y[p[i]]),(x[p[i+1]],y[p[i+1]])])
+        end
+        push!(rendered_paths,
+            compose(context(),
+                (context(),start,fill(color)),
+                (context(),goal,fill(color)),
+                (context(),line(rpath),stroke(color),linewidth(2pt))
+            )
+        )
+    end
+
+    compose(context(units=UnitBox(x₀, y₀, Δx, Δy)),
+        rendered_paths...,
+        compose(context(),circle(x,y,r*ones(length(x))),fill(color)),
+        compose(context(),line(edge_list),stroke(color),linewidth(4pt))
     )
 end
 

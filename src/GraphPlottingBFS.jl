ade1f0123bb9fc2ee947f0d0c7dfa05092d18cbf
kylebrown <kylejbrown17@gmail.com>
2020-02-27 17:39:28 -0800

    Almost done with overhaul of plotting functions.

--- src/GraphPlottingBFS.jl
+++ src/GraphPlottingBFS.jl
@@ -56,7 +56,7 @@ step_out(s::BFS_state) = BFS_state(d=s.d-1,w=s.w,d_max=s.d_max)
 
 function bfs!(G,v,s)
     s = step_in(s)
-    if length(inneighbors(G,v)) == 0
+    if indegree(G,v) == 0
         s = leaf_case!(G,v,s)
     else
         initial_branch_case!(G,v,s)
@@ -68,6 +68,92 @@ function bfs!(G,v,s)
     return step_out(s)
 end
 
+abstract type GraphInfoFeature end
+struct ForwardDepth <: GraphInfoFeature end
+struct BackwardDepth <: GraphInfoFeature end
+struct ForwardWidth <: GraphInfoFeature end
+struct BackwardWidth <: GraphInfoFeature end
+struct VtxWidth <: GraphInfoFeature end
+struct ForwardIndex <: GraphInfoFeature end
+struct ForwardCenter <: GraphInfoFeature end
+struct BackwardIndex <: GraphInfoFeature end
+struct BackwardCenter <: GraphInfoFeature end
+
+initial_value(f) = 1.0
+initial_value(f::Union{ForwardDepth,BackwardDepth}) = 1
+forward_propagate(f,G,v,val,v2,val2,args...) = val
+backward_propagate(f,G,v,val,v2,val2,args...) = val
+forward_accumulate(f,G,v,val,vtxs,vals,args...) = val
+backward_accumulate(f,G,v,val,vtxs,vals,args...) = val
+
+# forward_propagate(f::BackwardDepth,	G,v,val,v2,val2,args...) 	= max(val,val2-1)
+backward_propagate(f::BackwardDepth,G,v,val,v2,val2,args...) 	= max(val,val2+1)
+# backward_propagate(f::ForwardDepth,G,v,val,v2,val2,args...) 	= max(val,val2-1)
+forward_propagate(f::ForwardDepth,	G,v,val,v2,val2,args...) 	= max(val,val2+1)
+forward_propagate(f::ForwardWidth,	G,v,val,v2,val2,args...) 	= max(val,val2/max(1,outdegree(G,v2)))
+backward_propagate(f::BackwardWidth,G,v,val,v2,val2,args...) 	= max(val,val2/max(1,indegree(G,v2)))
+forward_accumulate(f::ForwardWidth,	G,v,val,vtxs,vals) 			= max(val,sum([0,map(i->vals[i]/outdegree(G,vtxs[i]),1:length(vals))...]))
+backward_accumulate(f::BackwardWidth,G,v,val,vtxs,vals) 		= max(val,sum([0,map(i->vals[i]/indegree(G,vtxs[i]),1:length(vals))...]))
+
+function get_graph_layout(G,feats=[ForwardDepth(),BackwardDepth(),ForwardWidth(),BackwardWidth()])
+	@assert !is_cyclic(G)
+	feat_vals = Dict(f=>map(v->initial_value(f),vertices(G)) for f in feats)
+	for v in topological_sort_by_dfs(G)
+		for (f,vec) in feat_vals
+			vec[v] = forward_accumulate(f,G,v,vec[v],inneighbors(G,v),map(v2->vec[v2], inneighbors(G,v)))
+			for v2 in inneighbors(G,v)
+				vec[v] = forward_propagate(f,G,v,vec[v],v2,vec[v2])
+			end
+		end
+	end
+	for v in topological_sort_by_dfs(reverse(G))
+		for (f,vec) in feat_vals
+			vec[v] = backward_accumulate(f,G,v,vec[v],outneighbors(G,v),map(v2->vec[v2], outneighbors(G,v)))
+			for v2 in outneighbors(G,v)
+				vec[v] = backward_propagate(f,G,v,vec[v],v2,vec[v2])
+			end
+		end
+	end
+	feat_vals[VtxWidth()] = max.(feat_vals[ForwardWidth()],feat_vals[BackwardWidth()])
+
+	graph = MetaDiGraph(deepcopy(G))
+	end_vtxs = [v for v in vertices(graph) if outdegree(graph,v) == 0]
+	s = BFS_state(0,0,0)
+	for v in end_vtxs
+		s = bfs!(graph,v,s)
+	end
+	for (idx_key,ctr_key,depth_key) in [(ForwardIndex(),ForwardCenter(),ForwardDepth()),((BackwardIndex(),BackwardCenter(),BackwardDepth()))]
+		vec = feat_vals[depth_key]
+		forward_width_counts = map(v->Int[],1:maximum(vec))
+		forward_idxs = zeros(nv(G))
+		backward_idxs = zeros(nv(G))
+		for v in topological_sort_by_dfs(G)
+			push!(forward_width_counts[vec[v]],v)
+		end
+		for vec in forward_width_counts
+			sort!(vec,by=v->get_prop(graph,v,:left))
+			for (i,v) in enumerate(vec)
+				if i > 1
+					v2 = vec[i-1]
+					forward_idxs[v] += forward_idxs[v2] + feat_vals[VtxWidth()][v2]
+				end
+				if indegree(G,v) > 0
+					min_idx = minimum(map(v2->forward_idxs[v2],inneighbors(G,v)))
+					forward_idxs[v] = max(forward_idxs[v],min_idx)
+					for v2 in vec[1:i-1]
+						forward_idxs[v] = max(forward_idxs[v],forward_idxs[v2]+feat_vals[VtxWidth()][v2])
+					end
+				end
+			end
+		end
+		feat_vals[idx_key] = forward_idxs
+		feat_vals[ctr_key] = forward_idxs .+ 0.5*feat_vals[VtxWidth()]
+	end
+
+	feat_vals
+end
+
+
 function get_graph_bfs(graph,v=0;
         shape_function = (G,v,x,y,r)->Compose.circle(x,y,r),
         color_function = (G,v,x,y,r)->"cyan",
@@ -75,29 +161,49 @@ function get_graph_bfs(graph,v=0;
         mode=:leaf_aligned,
         ϵ=0.000000001,
         edge_pad=1.1,
+		aspect_ratio=2,
         r = 0.2)
 
-    G = deepcopy(graph)
-    if v == 0
-        end_vtxs = [v for v in vertices(G) if length(outneighbors(G,v)) == 0]
-        s = BFS_state(0,0,0)
-        for v in end_vtxs
-            s = bfs!(G,v,s)
-        end
-    else
-        s = BFS_state(0,0,0)
-        s = bfs!(G,v,s)
-    end
-    set_default_graphic_size((s.d_max*2)cm,(s.w)cm)
+    # G = graph
+	feat_vals = get_graph_layout(graph)
     if mode == :leaf_aligned
-        x = [get_prop(G,v,:height)+0.5 for v in vertices(G)]
+        x = feat_vals[ForwardDepth()]
+	    y = feat_vals[ForwardCenter()]
     else
-        x = s.d_max .- [get_prop(G,v,:depth)-0.5 for v in vertices(G)]
+        x = feat_vals[BackwardDepth()]
+		x = 1 + maximum(x) .- x
+	    y = feat_vals[BackwardCenter()]
     end
-    y = [get_prop(G,v,:center) for v in vertices(G)]
+	x = x .+ (1 - minimum(x))
+	y = y .+ (1 - minimum(y))
+	# canvas_height = maximum(feat_vals[ForwardDepth()])+1
+	# canvas_width = maximum(feat_vals[ForwardWidth()])+1
+    # set_default_graphic_size((2*canvas_height)cm,(canvas_width)cm)
+	canvas_height = 1+maximum(x) .- min(0,minimum(x))
+	canvas_width = 1+maximum(y) .- min(0,minimum(y))
+    set_default_graphic_size((aspect_ratio*canvas_height)cm,(canvas_width)cm)
+
+    # G = MetaDiGraph(deepcopy(graph))
+    # if v == 0
+    #     end_vtxs = [v for v in vertices(G) if length(outneighbors(G,v)) == 0]
+    #     s = BFS_state(0,0,0)
+    #     for v in end_vtxs
+    #         s = bfs!(G,v,s)
+    #     end
+    # else
+    #     s = BFS_state(0,0,0)
+    #     s = bfs!(G,v,s)
+    # end
+    # set_default_graphic_size((s.d_max*2)cm,(s.w)cm)
+    # if mode == :leaf_aligned
+    #     x = [get_prop(G,v,:height)+0.5 for v in vertices(G)]
+    # else
+    #     x = s.d_max .- [get_prop(G,v,:depth)-0.5 for v in vertices(G)]
+    # end
+    # y = [get_prop(G,v,:center) for v in vertices(G)]
     rp = edge_pad*r; # padded radius for plotting
     lines = Vector{Compose.Form}()
-    for e in edges(G)
+    for e in edges(graph)
         dx = (x[e.dst] - x[e.src])
         dy = (y[e.dst] - y[e.src])
         d = sqrt(dx^2 + dy^2)
@@ -108,11 +214,11 @@ function get_graph_bfs(graph,v=0;
                 ])
         )
     end
-    shapes = map(i->shape_function(G,i,x[i],y[i],r),1:nv(graph))
-    fill_colors = map(i->color_function(G,i,x[i],y[i],r),1:nv(graph))
-    text_strings = map(i->text_function(G,i,x[i],y[i],r),1:nv(graph))
+    shapes = map(i->shape_function(graph,i,x[i],y[i],r),1:nv(graph))
+    fill_colors = map(i->color_function(graph,i,x[i],y[i],r),1:nv(graph))
+    text_strings = map(i->text_function(graph,i,x[i],y[i],r),1:nv(graph))
 
-    return s.d_max,s.w,x,y,lines,shapes,fill_colors,text_strings
+    return canvas_height,canvas_width,x,y,lines,shapes,fill_colors,text_strings
 end
 
 function display_graph_bfs(canvas_height,canvas_width,x,y,lines,shapes,fill_colors,text_strings)
@@ -124,9 +230,30 @@ function display_graph_bfs(canvas_height,canvas_width,x,y,lines,shapes,fill_colo
         (context(),lines...,stroke("black")))
 end
 
-function GraphPlottingBFS.plot_graph_bfs(graph,v=0;kwargs...)
+function plot_graph_bfs(graph,v=0;kwargs...)
     display_graph_bfs(get_graph_bfs(graph,v;kwargs...)...)
 end
+
+G = DiGraph(13)
+add_edge!(G,1,3)
+add_edge!(G,2,3)
+add_edge!(G,3,4)
+
+add_edge!(G,5,7)
+add_edge!(G,6,7)
+add_edge!(G,7,8)
+
+add_edge!(G,4,9)
+add_edge!(G,8,9)
+add_edge!(G,9,10)
+add_edge!(G,10,11)
+add_edge!(G,10,12)
+
+add_edge!(G,4,11)
+# add_edge!(G,7,4)
+
+plot_graph_bfs(G;mode=:root_aligned)
+plot_graph_bfs(G;mode=:leaf_aligned)
 
 """
     `SimpleGridWorld{G}`
